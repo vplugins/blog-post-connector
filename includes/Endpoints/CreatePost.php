d656f99cc70a0431d603043c03fa5332eb1afd16
Rajan Vijayan <rajanit2000@gmail.com>
2024-07-29 23:38:32 +0530

    update permission check using middleware

--- includes/Endpoints/CreatePost.php
+++ includes/Endpoints/CreatePost.php
@@ -2,8 +2,13 @@
 
 namespace VPlugins\SMPostConnector\Endpoints;
 
+use WP_REST_Request;
+use VPlugins\SMPostConnector\Middleware\AuthMiddleware;
+use VPlugins\SMPostConnector\Helper\Globals;
+
 class CreatePost {
     public function __construct() {
+        $this->auth_middleware = new AuthMiddleware();
         add_action('rest_api_init', [$this, 'register_routes']);
     }
 
@@ -11,7 +16,7 @@ class CreatePost {
         register_rest_route('sm-connect/v1', '/create-post', [
             'methods' => 'POST',
             'callback' => [$this, 'create_post'],
-            'permission_callback' => [$this, 'permissions_check']
+            'permission_callback' => [$this->auth_middleware, 'permissions_check']
         ]);
     }
 
@@ -19,7 +24,4 @@ class CreatePost {
         // Handle the post creation
     }
 
-    public function permissions_check($request) {
-        // Check user permissions
-    }
 }
