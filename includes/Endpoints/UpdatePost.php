f4e71cd731da965f4851bedfd1a4b64efbc93c0a
Rajan Vijayan <rajanit2000@gmail.com>
2024-08-06 01:09:22 +0530

    Update create / update classes extended with base post class

---
+++ includes/Endpoints/UpdatePost.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace VPlugins\SMPostConnector\Endpoints;
+
+class UpdatePost extends BasePost {
+    public function register_routes() {
+        register_rest_route('sm-connect/v1', '/update-post', [
+            'methods' => 'POST',
+            'callback' => [$this, 'update_post'],
+            'permission_callback' => [$this->auth_middleware, 'permissions_check']
+        ]);
+    }
+
+    public function update_post($request) {
+        return $this->handle_post_request($request, true);
+    }
+}
