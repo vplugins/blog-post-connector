14a84fb9bbbc4ac725b99dc8694a1f6c3e8360cc
Manish Yadav <msubachchanyadav@vendasta.com>
2024-10-17 19:35:12 +0530

    Removed functions to delete table

--- uninstall.php
+++ uninstall.php
@@ -30,32 +30,8 @@ function sm_remove_options() {
     foreach ($options as $option) {
         // Use delete_option() for regular options
         delete_option($option);
-
-        // Use $wpdb->query() to delete transient options, as they might be stored in a different manner
-        $wpdb->query($wpdb->prepare("DELETE FROM {$wpdb->options} WHERE option_name = %s", $option));
-    }
-}
-
-// Function to remove tables.
-function sm_remove_tables() {
-    global $wpdb;
-
-    // Specify the tables to be removed.
-    $tables = [
-        'sm_post_connector_token',
-        'sm_post_connector_default_post_type',
-        'sm_post_connector_default_author',
-        'sm_post_connector_default_category',
-        'sm_post_connector_secret_key',
-        'sm_post_connector_logo',
-    ];
-
-    // Loop through each table and drop it.
-    foreach ($tables as $table) {
-        $wpdb->query("DROP TABLE IF EXISTS $table");
     }
 }
 
 // Execute the removal functions.
 sm_remove_options();
-sm_remove_tables();
