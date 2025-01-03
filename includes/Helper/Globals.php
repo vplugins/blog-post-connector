c009b67ab8158d956f309cdd7aa267d7b433b258
Rajan Vijayan <rajanit2000@gmail.com>
2025-01-04 03:57:22 +0530

    Update live webhook URL

--- includes/Helper/Globals.php
+++ includes/Helper/Globals.php
@@ -13,7 +13,7 @@ class Globals {
      * @const string PLUGIN_VERSION The current version of the plugin.
      */
     const PLUGIN_VERSION = '1.0.1';
-    const WEBHOOK_URL = 'https://webhook.site/c9f73e3f-147a-4886-a0aa-3fb471b5c86a';
+    const WEBHOOK_URL = 'https://social-posts-prod.apigateway.co/vplugin/webhook/blog-post';
 
     /**
      * Retrieves the plugin slug.
